1bf7cda7884c3ebee0d39804de4605b9ee994f5f
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2018-07-05 23:37:12 -0500

    some swag doc

--- src/main/java/com/bhoya/pm/app/controllers/ProfanityMaskController.java
+++ src/main/java/com/bhoya/pm/app/controllers/ProfanityMaskController.java
@@ -3,10 +3,12 @@ package com.bhoya.pm.app.controllers;
 
 import com.bhoya.pm.app.models.ProfanityMask;
 import com.bhoya.pm.app.services.ProfaneMaskService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Api(value="profanitymask", description="Masks the profane found in input text excluding one in the whiltelist")
 public class ProfanityMaskController {
 
 
